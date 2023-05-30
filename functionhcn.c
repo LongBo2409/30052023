74b913266b5af1234b1ed96506a313409e82c37f
LongBo2409 <97206434+LongBo2409@users.noreply.github.com>
2023-05-30 08:37:21 +0700

    Update functionhcn.c(switchcase)

--- functionhcn.c
+++ functionhcn.c
@@ -10,10 +10,29 @@ int dientich(int a, int b){
 	return dientich;
 }
 int main(int cd, int cr){
+	int choice;
 	printf("Nhap vao chieu dai va chieu rong hinh chu nhat: \n");
 	scanf("%d%d", &cd, &cr);
-	printf("Chu vi hinh chu nhat la: %d\n", chuvi(cd, cr) );
-	printf("Dien tich hinh chu nhat la: %d\n", dientich(cd, cr));
+	printf("Chon lua tinh chu vi hay dien tich:\n");
+	printf("Chon 1 de tinh chu vi\n");
+	printf("Chon 2 de tinh dien tich\n");
+	scanf("%d", &choice);
+	
+	switch(choice){
+		case 1: 
+			printf("Chu vi hinh chu nhat la: %d\n", chuvi(cd, cr) );
+			break;
+				
+		case 2: 
+			printf("Dien tich hinh chu nhat la: %d\n", dientich(cd, cr));
+			break;
+			
+		default:
+			printf("Sai lua chon!");
+			break;
+	}
+	
 	return 0;
 }
+
 
